1454088b9e81107432cd4e9671f8d408accd5867
jonas mapuche <64318160+jonasMapuche@users.noreply.github.com>
2022-04-05 00:44:40 -0300

    Inclusão de leitor de colleção do mongo db e leitor de bancos do mongo db.

--- bin/connect.dart
+++ bin/connect.dart
@@ -1,3 +1,4 @@
+
 import 'package:skull_connect/calculate.dart' as untitled10;
 import 'package:mongo_dart/mongo_dart.dart';
 
@@ -5,4 +6,10 @@ void main (List<String> arguments) async {
   var db = await Db.create("mongodb+srv://jonas:freedown@cluster0.28oko.azure.mongodb.net/stomach?retryWrites=true&w=majority");
   await db.open();
   print('Connected to database: ${untitled10.calculate()}!');
+  DbCollection coll = db.collection('magic');
+  var lista = [];
+  var info = await db.getCollectionNames();
+  var stomach = await coll.find().toList();
+  print(stomach);
+  print(info);
 }
